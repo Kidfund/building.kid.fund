674f16fd83b3a972b3fdaf4b141c8901d8cd9459
Tim Broder <timothy.broder@gmail.com>
2017-05-19 13:21:17 -0400

    fix travis

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 bundle install
 bundle exec jekyll build
 cd _site
-rm Gemfile Gemfile.lock readme.md
+rm Gemfile Gemfile.lock README.md
 
 if [ $? -ne 0 ]; then echo "Could not generate the site"; exit 1; fi
 
